1306c90e2189358d6513e49f64eb891b1c5894bf
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-05-03 01:15:37 +0900

    動画30分進捗

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./TodoList";
+import { useState } from "react";
 
+//コンポーネントー＞役割分担しやすい
 function App() {
+  const [todos, setTodos] = useState(["Todo1","Todo2"]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todos ={todos}/>
+      <input type="text" />
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </>
   );
 }
+
 
 export default App;
---
+++ src/TodoList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Todo from './Todo'
+
+//コンポーネント
+export const TodoList = ({todos}) => {
+  return todos.map((todo) => <Todo todo = {todo} key = {todo}/>)
+}
+
+//外部でも使える
+export default TodoList;
